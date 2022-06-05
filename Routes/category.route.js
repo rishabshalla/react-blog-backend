bef6bb72fa1f0ae202bd418bb0cad21ea18fff23
rishabshalla <rishabshalla@gmail.com>
2022-06-05 13:59:41 +0530

    commit

---
+++ Routes/category.route.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const categoryRouter = express.Router();
+const categoryController = require("../Controllers/category.controllers");
+
+categoryRouter.route("/:cat").get(categoryController.category);
+
+module.exports = categoryRouter;
---
+++ Routes/home.route.js
@@ -0,0 +1,8 @@
+const express = require("express");
+
+const homeRouter = express.Router();
+const homeControllers = require("../Controllers/home.controllers");
+
+homeRouter.route("/").get(homeControllers.home);
+
+module.exports = homeRouter;
